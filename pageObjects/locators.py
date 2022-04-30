899727bf9cbb68cd7a62b3170abdaa0a7935817f
Dante Perez <danteprz96@gmail.com>
2022-04-30 14:55:06 -0500

    Adding locators file

---
+++ pageObjects/locators.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.by import By
+
+class saucelocators:
+    ##Login Page
+    username = (By.XPATH, "//input[@id='user-name']")
+    password = (By.XPATH,"//input[@id='password']")
+    login_btn = (By.XPATH,"//input[@id='login-button']")
+    logerror_msg = (By.XPATH,"//h3[@data-test='error']")
+
+    ##Product Page
+    item_name = (By.CLASS_NAME, "inventory_item_name")
+    addtocart_btn = (By.CLASS_NAME, "btn btn_primary btn_small btn_inventory")
+    sort_btn = (By.XPATH, "//select[@class='product_sort_container']")
+    lohi_opt = (By.XPATH, "//option[@value='lohi']")
+    hilo_opt = (By.XPATH, "//option[@value='hilo']")
+    cart_btn = (By.XPATH, "	//a[@class='shopping_cart_link']")
+    items_in_cart = (By.XPATH, "//span[@class='shopping_cart_badge']")
+    menu_btn = (By.XPATH, "//button[@id='react-burger-menu-btn']")
+    reset_app_menu = (By.XPATH, "//a[@id='reset_sidebar_link']")
+    logout_menu = (By.XPATH, "//a[@id='logout_sidebar_link']")
+    about_menu = (By.XPATH, "//a[@id='about_sidebar_link']")
+    twitter_btn = (By.XPATH, "//a[contains(text(),'Twitter')]")
+    fb_btn = (By.XPATH, "//a[contains(text(),'Facebook')]")
+    linkedin_btn = (By.XPATH, "//a[contains(text(),'LinkedIn')]")
+
+    ##Checkout Page
+    checkout_btn = (By.XPATH, "//button[@id='checkout']")
+    cart_item = (By.CLASS_NAME, "cart_item")
+
+    ##Delivery Page
+    first_name = (By.XPATH, "//input[@id='first-name']")
+    last_name = (By.XPATH, "//input[@id='last-name']")
+    postal_code = (By.XPATH, "//input[@id='postal-code']")
+    continue_btn = (By.XPATH, "//input[@id='continue']")
+
+    ##Overview Page
+    finish_btn = (By.XPATH, "//button[@id='finish']")
+
+    ##Complete Page
+    dispatched_text = (By.XPATH, "//div[@class='complete-text']")
